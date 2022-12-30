a68165239c48e504af71e363803423c9866804e0
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-12-30 19:11:54 +0100

    implementing home, search, list icons

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,40 @@
-const Sidebar = () => {
-    return ( 
-        <div>
-            <h1>Sidebar</h1>
-        </div>
+import {
+    HomeIcon,
+    MagnifyingGlassIcon,
+    ListBulletIcon,
     
-     );
+} from '@heroicons/react/24/outline'
+
+const Sidebar = () => {
+    return (
+      <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+        <h1>Sidebar</h1>
+        <div>
+          <button className='flex items-center space-x-2 
+          hover:text-purple-500' >
+            <HomeIcon className="h-5 w-5" />
+            <p>Home</p>
+          </button>
+        </div>
+
+        <div>
+          <button className='flex items-center space-x-2 hover:text-purple-500'>
+            <MagnifyingGlassIcon className="h-5 w-5" />
+            <p>Search</p>
+          </button>
+        </div>
+
+        <div>
+          <button className='flex items-center space-x-2 hover:text-purple-500'>
+            <ListBulletIcon className="h-5 w-5" />
+            <p>Your Library</p>
+          </button>
+          
+        </div>
+        <hr className='border-t-[0.1px] border-gray-700' />
+        
+      </div>
+    );
 }
  
 export default Sidebar;
