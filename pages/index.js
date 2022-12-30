32a4d82bde11058bb79d5a4927c59c729f2a4604
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-12-30 16:20:56 +0100

    importing my sidebar

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import Sidebar from '../components/Sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,13 +15,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className='Header'>
       <h1>Purple Music</h1>
       </div>
       <main>
         <h2>Your number one Purple music stream!</h2>
-        {/* {sidebar} */}
+        <Sidebar/>
         {/* {center} */}
       </main>
 
