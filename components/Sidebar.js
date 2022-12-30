fa1c053ae3624ffb005710606a3ebbe08daeb735
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-12-30 19:41:24 +0100

    adding some more Icons, and place holders fore the playlists

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,12 +2,14 @@ import {
     HomeIcon,
     MagnifyingGlassIcon,
     ListBulletIcon,
+    PlusIcon,
+    HeartIcon,
     
 } from '@heroicons/react/24/outline'
 
 const Sidebar = () => {
     return (
-      <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+      <div className='text-gray-500 p-5 text-sm border-r border-gray-900 space-y-5'>
         <h1>Sidebar</h1>
         <div>
           <button className='flex items-center space-x-2 
@@ -31,8 +33,34 @@ const Sidebar = () => {
           </button>
           
         </div>
-        <hr className='border-t-[0.1px] border-gray-700' />
-        
+        <hr className='border-t-[0.1px] border-gray-700' /> 
+
+        <div>
+          <button className='flex items-center space-x-2 
+          hover:text-purple-500' >
+            <PlusIcon className="h-5 w-5" />
+            <p>Create Playlist</p>
+          </button>
+        </div>
+
+        <div>
+          <button className='flex items-center space-x-2 hover:text-purple-500'>
+            <HeartIcon className="h-5 w-5" />
+            <p>The ones you love</p>
+          </button>
+        </div>
+
+        <div>
+          <button className='flex items-center space-x-2 hover:text-purple-500'>
+            <ListBulletIcon className="h-5 w-5" />
+            <p>Episodes</p>
+          </button>
+          
+        </div>
+        <hr className='border-t-[0.1px] border-gray-700' /> 
+
+        {/* playlist */}
+        <p className='cursor-pointer hover:text-purple-500'>Playlist name...</p>
       </div>
     );
 }
