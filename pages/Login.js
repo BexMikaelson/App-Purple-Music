21e5d08f0927bed686c94c6c32e6112d86209ba1
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-03 17:16:25 +0100

    adding the MusicalNoteIcon to the login page

--- pages/Login.js
+++ pages/Login.js
@@ -1,11 +1,16 @@
 import { getProviders, signIn } from "next-auth/react";
+import {MusicalNoteIcon} from '@heroicons/react/24/outline';
 
 const Login = () => {
-    return ( 
-        <div>
-            <h1>Login</h1>
+    return (
+      <div>
+        <h1>Login</h1>
+
+        <div className="flex place-content-center text-purple-700 ">
+          <MusicalNoteIcon className="h-80 w-80" />
         </div>
-     );
+      </div>
+    );
 }
  
 export default Login;
@@ -16,8 +21,8 @@ export async function getServerSideProps(){
 
     return {
         props: {
-            providers
+            providers,
         }
-    }
+    };
 
 }
