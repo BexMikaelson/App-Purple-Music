571ab5ba186c625138d92efc5e8c9151d4ab83b4
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-03 16:46:31 +0100

    returning: props: provider

--- pages/Login.js
+++ pages/Login.js
@@ -14,4 +14,10 @@ export default Login;
 export async function getServerSideProps(){
     const providers = await getProviders();
 
+    return {
+        props: {
+            providers
+        }
+    }
+
 }
