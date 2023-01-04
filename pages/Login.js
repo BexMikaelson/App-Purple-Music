a08f68581fe76915e1e601593e9dc2e559428f11
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-04 15:54:50 +0100

    using object.values on my spotify provider to get the "Sign in with Spotify"

--- pages/Login.js
+++ pages/Login.js
@@ -1,15 +1,25 @@
 import { getProviders, signIn } from "next-auth/react";
 import {MusicalNoteIcon} from '@heroicons/react/24/outline';
 
-const Login = () => {
+const Login = ({providers}) => {
     return (
       <div>
         <h1>Login</h1>
-
-        <div className="flex place-content-center text-purple-700 ">
+        <div className="flex place-content-center text-purple-700">
           <MusicalNoteIcon className="h-80 w-80" />
         </div>
-      </div>
+        <div className="flex place-content-center py-20">
+          { Object.values(providers).map((provider) => (
+              <div className="bg-purple-700 p-5 rounded-full " key={provider.name}>
+                   <button onClick={() => signIn(provider.id)}>
+                        Sign in with {provider.name}
+                    </button>
+
+              </div>
+          )
+          )}
+        </div>
+    </div>
     );
 }
  
@@ -22,7 +32,7 @@ export async function getServerSideProps(){
     return {
         props: {
             providers,
-        }
+        },
     };
 
 }
