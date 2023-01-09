00aa12a9b8c78172782e83943324e95b8726ca97
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-09 21:21:11 +0100

    styling the section

--- components/Center.js
+++ components/Center.js
@@ -6,19 +6,23 @@ const Center = () => {
     console.log(session)
 
     return ( 
-        <div className="flex flex-grow">
+        <div className=" flex-grow">
         
-            <header>
-                <div className="flex items-center space-x-3  opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 bg-purple-900">
-                    <img className="rounded-full w-10 h-10 bg-black " src={session?.user.image} alt="" />
+            <header className="absolute top-5 right-8">
+                <div className="flex items-center space-x-3  opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 bg-black">
+                    <img className="rounded-full w-10 h-10 bg-purple-900 " src={session?.user.image} alt="" />
 
                     <div className="flex items-center space-x-2 hover:text-purple-500"> 
                         <h2> {session?.user.name} </h2>
                         <ChevronDownIcon className="w-5 h-5"/>
                     </div>
                  </div>
-
             </header>
+
+            <section className={`flex items-end space-x-7 bg-gradient-to-b to-black from-purple-900 h-80 padding-8`}>
+                <h1>hello</h1>
+
+            </section>
         </div>
      );
 }
