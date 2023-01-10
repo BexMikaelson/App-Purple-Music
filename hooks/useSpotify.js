1dac984ecc9b1a9f12cd3d9d4e3945c8e82d2b4c
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-10 13:08:03 +0100

    fix: SpotifyWebApi import

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,7 +1,7 @@
 import { signOut } from "next-auth/react";
 import { useEffect } from "react";
 import { signIn, useSession} from "next-auth/react";
-import spotifyApi from "../lib/spotify";
+import SpotifyWebApi from "spotify-web-api-node";
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
