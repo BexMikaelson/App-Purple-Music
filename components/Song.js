0ad86c864ae26f0308ca73d06d176fee287f8a68
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-11 14:24:45 +0100

    geting the songs with order, image, name and artist name

--- components/Song.js
+++ components/Song.js
@@ -1,8 +1,29 @@
-const Song = ( {order, track} ) => {
-    return ( 
-    <div>
+import useSpotify from "../hooks/useSpotify";
 
-    </div> );
+const Song = ( {order, track} ) => {
+    const spotifyApi = useSpotify()
+    return (
+      <div className="">
+        <div className="flex items-center space-x-4 p-2">
+          <p> {order + 1} </p>
+          <img
+            className="w-20 "
+            src={track.track.album.images[0].url}
+            alt="song cover image"
+          />
+
+          <div>
+            <p> {track.track.name} </p>
+            <p> {track.track.artists[0].name} </p>
+          </div>
+        </div>
+
+        <div>
+            <p> {} </p>
+            <p></p>
+        </div>
+      </div>
+    );
 }
  
 export default Song;
