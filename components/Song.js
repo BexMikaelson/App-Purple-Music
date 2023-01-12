4c22d84e769f0ae757241629d0f0a0d6969d3842
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-12 11:24:53 +0100

    adding timeduration

--- components/Song.js
+++ components/Song.js
@@ -1,10 +1,11 @@
 import useSpotify from "../hooks/useSpotify";
+import millisToMinutesAndSeconds from "../lib/timeDuration"
 
 const Song = ( {order, track} ) => {
     const spotifyApi = useSpotify()
     return (
       <div className="">
-        <div className="flex items-center space-x-4 p-2">
+        <div className="flex items-center  space-x-4 p-2">
           <p> {order + 1} </p>
           <img
             className="w-20 "
@@ -15,13 +16,21 @@ const Song = ( {order, track} ) => {
           <div>
             <p> {track.track.name} </p>
             <p> {track.track.artists[0].name} </p>
+            
+            
           </div>
+          {/* <div className="flex items-center justify-between ml-auto md:ml-0">
+          <p className="hidden md:inline "> {track.track.album.name} </p>
+          </div> */}
         </div>
+        
 
-        <div>
-            <p> {} </p>
-            <p></p>
-        </div>
+        <div className="flex items-center justify-between ml-auto md:ml-0">
+          <p className="hidden md:inline "> {track.track.album.name} </p>
+          <p> {millisToMinutesAndSeconds(track.track.duration_ms)} </p>
+          </div>
+
+        
       </div>
     );
 }
