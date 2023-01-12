97dc4f787fb09f3dbbd63373c87712fc95750809
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-12 12:51:53 +0100

    adding useRecoilState with currentTrackIdState and isPlayingState to use to playing song

--- components/Song.js
+++ components/Song.js
@@ -1,8 +1,12 @@
+import { useRecoilState } from "recoil";
 import useSpotify from "../hooks/useSpotify";
 import millisToMinutesAndSeconds from "../lib/timeDuration"
+import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 
 const Song = ( {order, track} ) => {
     const spotifyApi = useSpotify()
+    const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
+    const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
     return (
       <div className="text-gray-500 hover:bg-gray-900 rounded-lg cursor-pointer">
         <div className="flex items-center  space-x-4 py-5 px-5 ">
