4567035e9af3d1ccb3fa46ac589dd2e5627d925e
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-13 14:28:09 +0100

    adding styling

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -16,7 +16,7 @@ const MusicPlayer = () => {
     const fetchCurrentSong = () =>{
         if(!songInfo) {
             spotifyApi.getMyCurrentPlayingTrack().then((data) => {
-                console.log('playing: ', data.body?.item);
+                // console.log('playing: ', data.body?.item);
                 setCurrentTrackId(data.body?.item?.id);
 
                 spotifyApi.getMyCurrentPlaybackState().then((data)=> {
@@ -31,16 +31,29 @@ const MusicPlayer = () => {
     useEffect(()=> {
         if (spotifyApi.getAccessToken() && !currentTrackId){
             //fetch the song info
+            // fetchCurrentSong();
+            setVolume(50);
         }
 
     },[currentTrackIdState, spotifyApi, session])
 
-    return ( 
-        <div>
-            <h1>player</h1>
-            <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
+    return (
+      <div className="h-24 bg-gradient-to-b from-black to-purple-900 grid grid-cols-3 text-xs md:text-base px-2 md:px-8">
+        {/* <h1>player</h1> */}
+        <div className="flex items-center space-x-4">
+          <img
+            className="hidden md:inline h-10 w-10"
+            src={songInfo?.album.images?.[0]?.url}
+            alt=""
+          />
+
+          <div>
+            <h3> {songInfo?.name} name</h3>
+            <p> {songInfo?.artists?.[0]?.name} artist</p>
+          </div>
         </div>
-     );
+      </div>
+    );
 }
  
 export default MusicPlayer;
