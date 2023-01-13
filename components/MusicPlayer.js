0efee7cdb930110fc48ceb897e22b6b43ffe1b6a
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-13 12:57:50 +0100

    geting the song image when song is plaing

--- components/MusicPlayer.js
+++ components/MusicPlayer.js
@@ -3,6 +3,7 @@ import {useSession} from "next-auth/react";
 import { currentTrackIdState, isPlayingState } from "../atoms/songAtom";
 import { useRecoilState } from "recoil";
 import { useState } from "react";
+import useSongInfo from "../hooks/useSongInfo";
 
 const MusicPlayer = () => {
     const spotifyApi = useSpotify();
@@ -10,10 +11,15 @@ const MusicPlayer = () => {
     const [currentTrackId, setCurrentTrackId] = useRecoilState(currentTrackIdState);
     const [isPlaying, setIsPlaying] = useRecoilState(isPlayingState);
     const[volume, setVolume] = useState(50)
+    const songInfo = useSongInfo();
+
+    //useEffekt to uppdate the musicplayer+ playerimg on reload if a song is alredy playing
+    
+
     return ( 
         <div>
             <h1>player</h1>
-            <img src="" alt="" />
+            <img className="hidden md:inline h-10 w-10" src={songInfo?.album.images?.[0]?.url} alt="" />
         </div>
      );
 }
