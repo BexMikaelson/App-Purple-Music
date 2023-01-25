3780c2d48a694a71f841f638c5c5db70a38ec45e
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-25 20:21:36 +0100

    fix clean up

--- components/Songs.js
+++ components/Songs.js
@@ -6,7 +6,7 @@ const Songs = () => {
     const playlist = useRecoilValue(playlistState)
     return ( 
     <div className="px-8 flex flex-col space-y-1 pb-28">
-        <h1>songs</h1>
+        {/* <h1>songs</h1> */}
     
         {playlist?.tracks.items.map((track, i) =>(
             <Song key={track.track.id} track={track} order={i} />
