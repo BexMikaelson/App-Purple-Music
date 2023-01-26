1a09a12e517203906ee25f8b8729d3b50f400807
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-26 14:46:11 +0100

    using Recoil to get the playlist and songs

--- components/Songs.js
+++ components/Songs.js
@@ -1,18 +1,29 @@
 import { useRecoilValue } from "recoil";
 import { playlistState } from "../atoms/playlistAtom";
+import { albumTracks } from "../atoms/songAtom";
 import Song from "../components/Song"
 
-const Songs = () => {
-    const playlist = useRecoilValue(playlistState)
-    return ( 
-    <div className="px-8 flex flex-col space-y-1 pb-28">
-        {/* <h1>songs</h1> */}
-    
-        {playlist?.tracks.items.map((track, i) =>(
-            <Song key={track.track.id} track={track} order={i} />
-        ))}
+const Songs = ({ songs = [] }) => {
+    return (
+        <div className="px-8 flex flex-col space-y-1 pb-28">
+            {/* <h1>songs</h1> */}
+            {songs.map((track, i) => (
+                <Song key={track.track.id} track={track} order={i} />
+            ))}
 
-    </div> );
+        </div>);
 }
- 
+
+export const PlaylistSongs = () => {
+    const playlist = useRecoilValue(playlistState)
+    return <Songs songs={playlist?.tracks.items} />;
+}
+
+export const AlbumSongs = () => {
+    const songs = useRecoilValue(albumTracks)
+    return <Songs songs={songs.map(song => ({ track: song }))} />;
+}
+
+
+
 export default Songs;
