a5be79da407c11ae9e8b7bd64be6b5d41b431d8b
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-26 17:22:37 +0100

    clean up

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -37,13 +37,6 @@ const Sidebar = () => {
     <div className='flex flex-col text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 space-y-5 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex '>
       <h1 className='font-bold text-purple-600'>Purple Music</h1>
 
-      {/* <div>
-          <button className='flex items-center space-x-2
-          hover:text-purple-500' onClick={()=> signOut()} >
-
-            <p>Sign Out</p>
-          </button>
-        </div> */}
 
       <div>
         <button className='flex items-center space-x-2
