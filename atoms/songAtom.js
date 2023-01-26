88dd939222862c041d5e9bbb9d045b98981a597f
Heidi sjöberg <heidi.alexx8@gmail.com>
2023-01-26 14:39:52 +0100

    adding albumTracks

--- atoms/songAtom.js
+++ atoms/songAtom.js
@@ -1,11 +1,16 @@
-import {atom} from "recoil";
+import { atom } from "recoil";
 
 export const currentTrackIdState = atom({
-    key: 'currentTrackIdState', // unique ID (with respect to other atoms/selectors)
-    default: null, // default value (aka initial value)
-  });
+  key: 'currentTrackIdState', // unique ID (with respect to other atoms/selectors)
+  default: null, // default value (aka initial value)
+});
 
 export const isPlayingState = atom({
-    key: 'isPlayingState', // unique ID (with respect to other atoms/selectors)
-    default: false, // default value (aka initial value)
-  });
+  key: 'isPlayingState', // unique ID (with respect to other atoms/selectors)
+  default: false, // default value (aka initial value)
+});
+
+export const albumTracks = atom({
+  key: 'albumTracks',
+  default: [], // default value (aka initial value)
+});
